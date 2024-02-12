2f0a3e9e93890f26e8500aac7a9a77e701526829
Fernando <fparmezani@hotmail.com>
2024-02-12 08:39:06 -0300

    delete project

---
+++ server/config.js
@@ -0,0 +1,19 @@
+var config = {
+  dev: {
+    url: "http://localhost/",
+    port: 3000,
+    ambiente: "DEV",
+    database: {
+      host: "delivery-feparmezani.a.aivencloud.com",
+      port: "23663",
+      user: "avnadmin",
+      password: "AVNS_k6EotLv50qRf5k-Lwvj",
+      database: "pizzaria",
+    },
+  },
+};
+exports.get = function get(ambiente) {
+  if (ambiente.toLowerCase() === "dev") {
+    return config.dev;
+  }
+};

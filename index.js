c75096376a0cc76c6508ff177ba60d6a07eaa4c9
Fernando <fparmezani@hotmail.com>
2024-02-12 08:50:45 -0300

    delete project

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const { server, restify, config } = require('./server/server');
-
-server.get('/*', restify.plugins.serveStatic({
-    directory: './client',
-    default: 'index.html'
-}))
-
+const { server, restify, config } = require("./server/server");
+server.get(
+  "/*",
+  restify.plugins.serveStatic({
+    directory: "./client",
+    default: "index.html",
+  })
+);
 server.listen(config.port || 3000, () => {
-    console.log(`AMBIENTE: ${config.ambiente} URL: ${config.url} PORTA: ${config.port}`)
-})
+  console.log(
+    `AMBIENTE: ${config.ambiente} URL: ${config.url} PORTA: ${config.port}`
+  );
+});

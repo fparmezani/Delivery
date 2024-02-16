860c4c00940d32fce953b569fda69280bfe0cf69
Fernando <fparmezani@hotmail.com>
2024-02-16 11:03:04 -0300

    delete project

--- server/config.js
+++ server/config.js
@@ -11,10 +11,26 @@ var config = {
       database: "pizzaria", // Variável de ambiente para o nome do banco de dados
     },
   },
+  prod: {
+    url: "http://www.menumagic.com.br",
+    port: 21034,
+    ambiente: "PROD",
+    database: {
+      host: "delivery-feparmezani.a.aivencloud.com", // Variável de ambiente para o endereço do servidor
+      port: "23663", // Variável de ambiente para a porta
+      user: "avnadmin", // Variável de ambiente para o nome de usuário
+      password: "AVNS_k6EotLv50qRf5k-Lwvj", // Variável de ambiente para a senha
+      database: "pizzaria", // Variável de ambiente para o nome do banco de dados
+    },
+  },
 };
 
 exports.get = function get(ambiente) {
   if (ambiente.toLowerCase() === "dev") {
     return config.dev;
+  }
+
+  if (ambiente.toLowerCase() === "prod") {
+    return config.prod;
   }
 };
